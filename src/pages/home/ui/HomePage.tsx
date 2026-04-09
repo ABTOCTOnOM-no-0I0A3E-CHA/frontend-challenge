8e4bdd6f54d934c6c89b0e7ffbab60d0fd810ebc
ABTOCTOnOM-no-0I0A3E-CHA <l4pk@icloud.com>
2026-04-09 15:52:08 +0300

    feat: homepage layout

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,7 +1,19 @@
 export function HomePage() {
     return (
-        <>
-            <h1 className="text-3xl font-bold underline">Testing text</h1>
-        </>
+        <main>
+            <section
+                className="pt-12 px-15.25 gap-12 grid"
+                style={{
+                    gridTemplateColumns:
+                        "repeat(auto-fill, minmax(13rem, 1fr))",
+                }}
+            >
+                <div className="w-56.25 aspect-square hover:scale-[1.13776] transition-all duration-200 bg-black"></div>
+                <div className="w-56.25 aspect-square hover:scale-[1.13776] transition-all duration-200 bg-black"></div>
+                <div className="w-56.25 aspect-square hover:scale-[1.13776] transition-all duration-200 bg-black"></div>
+                <div className="w-56.25 aspect-square hover:scale-[1.13776] transition-all duration-200 bg-black"></div>
+                <div className="w-56.25 aspect-square hover:scale-[1.13776] transition-all duration-200 bg-black"></div>
+            </section>
+        </main>
     );
 }
